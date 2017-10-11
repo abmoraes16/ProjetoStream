0780d9a2b8e59c2afc3019854b69a936cee18bd1
FIC <a.bmoraes@outlook.com>
2017-10-10 21:54:40 -0300

    StreamWriter

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ProjetoStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Teste.csv",true);
+
+            sw.WriteLine(Console.ReadLine());
+
+            sw.Close();
+
+            FileInfo fi = new FileInfo("Teste.csv");
+
+            if (fi.Exists){
+                Console.WriteLine("Tamanho: " +fi.Length);
+                Console.WriteLine("Extensão: " +fi.Extension);
+                Console.WriteLine("Nome: " +fi.FullName);
+            }
+            else
+                Console.WriteLine("Arquivo não existe");
+            
+        }
+    }
+}
